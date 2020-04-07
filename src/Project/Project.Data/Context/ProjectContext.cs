eca4690524861b379c8afe703e4881712bedf2f5
Abdullah ILGAZ <mail@abdullahilgaz.com>
2020-04-07 14:44:02 +0300

    Create ProjectContext.cs

---
+++ src/Project/Project.Data/Context/ProjectContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Data.Context
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext()
+            :base("name=ProjectEntities")
+        {
+
+        }
+    }
+}
