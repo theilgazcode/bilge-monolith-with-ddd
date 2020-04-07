5d63ff1eda73cc7ab158260dbb6eeb9bd5b6150a
Abdullah ILGAZ <mail@abdullahilgaz.com>
2020-04-07 14:38:58 +0300

    Create Enums.cs

---
+++ src/Project/Project.Core/Enums/Enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Core.Enums
+{
+    public enum Status
+    {
+        Active,
+        Inactive,
+        Closed,
+        Completed,
+        Waiting
+    }
+
+    public enum UserType
+    {
+        Administrator,
+        SuperAdministrator,
+        User,
+        Reporter
+    }
+}
